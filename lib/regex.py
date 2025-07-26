20352fcbad3379cbed9445b1588382ea90272b82
mathiasmunene <mathiasmunene@gmail.com>
2025-07-26 06:02:33 +0300

    final commit

--- lib/regex.py
+++ lib/regex.py
@@ -1,13 +1,18 @@
 import re
 
-# NOTE: There are only a few tests included, so multiple solutions will work.
-# Feel free to encourage students to find oversights and add tests to this lab!
+# Name regex - matches capitalized names with spaces, hyphens, or apostrophes
+name_regex = re.compile(r'^[A-Z][A-Za-z\'-]+( [A-Z][A-Za-z\'-]+)*$')
+print(name_regex.fullmatch("Anya Taylor-Joy"))  # Should match
+print(name_regex.fullmatch("John O'Connor"))    # Should match
+print(name_regex.fullmatch("123 John"))         # Should NOT match
 
-name = r""
-name_regex = re.compile(name)
+# Phone regex - matches various phone number formats
+phone_regex = re.compile(r'^(\+?\d{1,3}[-.\s]?)?\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}$')
+print(phone_regex.fullmatch("(555) 555-5555"))  # Should match
+print(phone_regex.fullmatch("555-555-5555"))    # Should match
+print(phone_regex.fullmatch("+1 555 555 5555")) # Should match
 
-phone_number = r""
-phone_regex = re.compile(phone_number)
-
-email_address = r""
-email_regex = re.compile(email_address)
+# Email regex - standard email validation
+email_regex = re.compile(r'^[A-Za-z][A-Za-z0-9._%+-]*@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$')
+print(email_regex.fullmatch("user@gmail.com"))    # Should match
+print(email_regex.fullmatch("invalid@email"))    # Should NOT match
